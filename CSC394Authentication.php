9933271eb28652152fe3ca79172048c7bcc9bcbb
Kevin Rama <basedgodrama@loop-depaulsecure-241-128.depaulsecure-student.depaul.edu>
2018-05-16 18:57:04 -0500

    Added authentication

--- CSC394Authentication.php
+++ CSC394Authentication.php
@@ -1,53 +1,50 @@
 <?php
 include_once("dbcontroller.php");
 
-function hashPassword(){
-
     $db_handle = new DBController();
     $conn = $db_handle->connectDB();
-    $result = runQuery($conn,"SELECT * FROM users WHERE email='" . $_POST["email"] . "' and password = '". $_POST["password"]."'");
+    $result = runQuery($conn,"SELECT * FROM users WHERE EMAIL='" . $_POST['email'] . "' and PASSWORD = '". $_POST['password']."'");
     $count  = numRows($result);
-    $hash = password_hash($password, PASSWORD_BCRYPT, $result);
+    $email = $result["email"];
+    $hash = password_hash($password, PASSWORD_BCRYPT, $result['PID']);
 
-}
-
-function check(){
-    if(isset($_POST['submit'])) {
-	    if($count==0) {
+    if(isset($_POST['submit'])){
+        if($count==0) {
             $message = "Invalid Username or Password!";
             header("Location: ../index.php");
-        } 
-
-        else if($hash==$result['password']&& $result['role']=='student'){
-            $_SESSION['s_id']=$result['Student_ID'];
-            $_SESSION['s_first']=$result['First_Name'];
-            $_SESSION['s_last']=$result['Last_Name'];
-            $_SESSION['s_email']=$result['Student_Email'];
-            $_SESSION['s_major']=$result['Majors'];
-            header("Location: ../home.php");
-            exit();
         }
-
-        else if($hash==$result['password']&& $result['role']=='admin'){
-            $_SESSION['a_id']=$result['Admin_ID'];
-            $_SESSION['a_first']=$result['First_Name'];
-            $_SESSION['a_last']=$result['Last_Name'];
-            $_SESSION['a_email']=$result['Admin_Email'];
-            $_SESSION['a_major']=$result['Majors'];
-            header("Location: ../adminhome.php");
-            exit();
+        else{
+            if($row=mysqli_fetch_assoc($result)){
+                if(password_verify($result['PASSWORD'], $hash) && $result['PID']=="STU"){
+                    $_SESSION["email"]= $email;
+                    $_SESSION['s_id']=$row['ID'];
+                    $_SESSION['s_first']=$row['FNAME'];
+                    $_SESSION['s_last']=$row['LNAME'];
+                    $_SESSION['s_major']=$row['MAJOR'];
+                    header("Location: ../home.php");
+                    exit();
+                }
+                elseif(password_verify($result['PASSWORD'], $hash) && $result['PID']=="ADM"){
+                    $_SESSION["email"]= $email;
+                    $_SESSION['a_id']=$row['ID'];
+                    $_SESSION['a_first']=$row['FNAME'];
+                    $_SESSION['a_last']=$row['LNAME'];
+                    header("Location: ../admin/home.php");
+                    exit();
+                }
+                elseif(password_verify($result['PASSWORD'], $hash) && $result['PID']=="FAC"){
+                    $_SESSION["email"]= $email;
+                    $_SESSION['f_id']=$row['ID'];
+                    $_SESSION['f_first']=$row['FNAME'];
+                    $_SESSION['f_last']=$row['LNAME'];
+                    $_SESSION['f_dept']=$row['Dept'];
+                    header("Location: ../faculty/home.php");
+                    exit();
+                }
+            }
         }
-        else if($hash==$result['password']&& $result['role']=='faculty'){
-            $_SESSION['f_id']=$result['Admin_ID'];
-            $_SESSION['f_first']=$result['First_Name'];
-            $_SESSION['f_last']=$result['Last_Name'];
-            $_SESSION['f_email']=$result['Admin_Email'];
-            $_SESSION['f_major']=$result['Majors'];
-            header("Location: ../facultyhome.php");
-            exit();
-        }
-	}
-}
-
+            
+    }
+            
 
 ?>
