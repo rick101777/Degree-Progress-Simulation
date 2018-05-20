61833ea4cdf88526b5c0a5b4a9c2b4af55b89c6b
brandon1202h <39170216+brandon1202h@users.noreply.github.com>
2018-05-20 15:05:04 -0500

    Add files via upload

--- faculty/progress.php
+++ faculty/progress.php
@@ -28,8 +28,29 @@
         <div class="space-2"></div>
         <button type="submit" class="btn btn-primary" name="submit">Submit</button>
       </div>
-      <div class="space-3"></div>
     </form>
+
+    <div class="space-4"></div>
+
+    <div class = "section-a">
+      <div class = "container">
+        <div class="row">
+          <div class ="col-md-6">
+            <i class="fas fa-phone"></i>
+            <h3>Contact</h3>
+            <p>Vestibulum neque erat, semper elementum justo posuere, porta eleifend quam. Cras viverra ultricies lacinia. Nulla elementum ligula sed nunc finibus commodo. Curabitur efficitur velit mauris, pulvinar volutpat tortor egestas commodo. Praesent eget ante commodo, iaculis </p>
+            <a href="contact.php" class="btn btn-primary">GO!</a>
+          </div>
+
+          <div class ="col-md-6">
+            <i class="fas fa-id-card"></i>
+            <h3>Profile</h3>
+            <p>Vestibulum neque erat, semper elementum justo posuere, porta eleifend quam. Cras viverra ultricies lacinia. Nulla elementum ligula sed nunc finibus commodo. Curabitur efficitur </p>
+            <a href="profile.php" class="btn btn-primary">GO!</a>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <script src="../bower_components/jquery/dist/jquery.js"></script>
     <script src="../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js"></script>
