2c08f633b5491459c1907976f72a8d102cf85800
Michael <szalmichael@gmail.com>
2018-05-23 15:18:07 -0500

    Added example heading for progress report

---
+++ progress_report.php
@@ -0,0 +1,49 @@
+<?php $title = "Degree Progress Report";?>
+<?php require_once ("header.php"); ?>
+
+<html lang="en">
+
+    <head>
+        <link rel="stylesheet" href="css/report.css">
+    </head>
+    
+    <body>
+        <div class="title-bar">
+            <h1>Degree Progress Report
+        </div>
+
+        <div class="space"></div>
+
+        <h2>Autumn 2018</h2>
+        <table class='table' border = '1'>
+            <tr>
+            <th scope='col'>Course ID</th>
+            <th scope='col'>Subject</th>
+            <th scope='col'>Cat Number</th>
+            <th scope='col'>Title</th>
+            <th scope='col'>Consent</th>
+            <th scope='col'>Subject_Desc</th>
+            <th scope='col'>Location</th>
+            </tr>
+
+            <tr>
+            <td>???</td>
+            <td>???</td>
+            <td>???</td>
+            <td>???</td>
+            <td>???</td>
+            <td>???</td>
+            <td>???</td>
+            </tr>
+    
+        </table>
+        
+        <div class="space2"></div>
+        <button type="submit" class="btn btn-secondary" name="submit">Save Report</button>
+    </body>
+    
+
+    
+</html>    
+
+<?php require_once ("footer.php"); ?>
