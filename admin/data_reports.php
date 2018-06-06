7d73bf4bb64e421648c5cd8aa21179c31d90ec77
Kevin Rama <kevinrama23@gmail.com>
2018-06-06 17:36:42 -0500

    fixed directory

--- admin/data_reports.php
+++ admin/data_reports.php
@@ -10,40 +10,40 @@
       <div class = "img-container">
         <div class = "col-md-8">
           <h1 class="h3 mb-3 font-weight-normal">Enrollment</h1>
-          <img src="img/Enrollment.jpg"width=100% height=100%>
-          <img src="img/BarGraph.jpg"width=100% height=100%>
+          <img src="../img/Enrollment.jpg"width=100% height=100%>
+          <img src="../img/BarGraph.jpg"width=100% height=100%>
           <br>
           <br>
           <h1 class="h3 mb-3 font-weight-normal">Quarters</h1>
-          <img src="img/Quarters.jpg"width=100% height=100%>
+          <img src="../img/Quarters.jpg"width=100% height=100%>
           <br>
           <br>
           <h1 class="h3 mb-3 font-weight-normal">Business Analysis</h1>
-          <img src="img/BusinessAnalysis.jpg"width=100% height=100%>
+          <img src="../img/BusinessAnalysis.jpg"width=100% height=100%>
           <br>
           <br>
           <h1 class="h3 mb-3 font-weight-normal">Business Intelligence Count</h1>
-          <img src="img/BusinessIntelligence.jpg"width=100% height=100%>
+          <img src="../img/BusinessIntelligence.jpg"width=100% height=100%>
           <br>
           <br>
           <h1 class="h3 mb-3 font-weight-normal">Database Admin Count</h1>
-          <img src="img/DatabaseAdmin.jpg"width=100% height=100%>
+          <img src="../img/DatabaseAdmin.jpg"width=100% height=100%>
           <br>
           <br>
           <h1 class="h3 mb-3 font-weight-normal">IT Enterprise Management Count</h1>
-          <img src="img/IT.jpg"width=100% height=100%>
+          <img src="../img/IT.jpg"width=100% height=100%>
           <br>
           <br>
           <h1 class="h3 mb-3 font-weight-normal">Standard Count</h1>
-          <img src="img/StandardCount.jpg"width=100% height=100%>
+          <img src="../img/StandardCount.jpg"width=100% height=100%>
           <br>
           <br>
           <h1 class="h3 mb-3 font-weight-normal">Number of Courses</h1>
-          <img src="img/DataBarVisual.jpg"width=100% height=100%>
+          <img src="../img/DataBarVisual.jpg"width=100% height=100%>
           <br>
           <br>
           <h1 class="h3 mb-3 font-weight-normal">Pre-Req Count</h1>
-          <img src="img/PreReqCount.jpg"width=100% height=100%>
+          <img src="../img/PreReqCount.jpg"width=100% height=100%>
   </body>
 
   <script src="../bower_components/jquery/dist/jquery.js"></script>
